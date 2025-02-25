5b79ef481b7b8883b3c12640b35bcfe05901028a
Ansh sharma <anshshama8nbi@gmail.com>
2025-02-25 14:55:58 +0530

    Add .py file for text to audio

--- text2audio.py
+++ text2audio.py
@@ -1,10 +1,10 @@
 from gtts import gTTS
 import os
 
-def text_to_audio(text, filename="Non-Biodegradable.mp3"):
+def text_to_audio(text, filename="liquid.mp3"):
     tts = gTTS(text=text, lang="en")
     tts.save(filename)
     os.system(f"start {filename}")  # For Windows; use `afplay` on Mac, `mpg321` on Linux
 
-text = "Your Non-Biodegradable Waste Bin is full. Please empty it."
+text = "Your Liquid Waste Bin is full. Please empty it."
 text_to_audio(text)
